6705ec5f2c95179e2d77d80176b3d47d2698e243
ruben bras <ru3enbras@gmail.com>
2025-03-31 17:47:31 +0100

    ct

--- src/registo.php
+++ src/registo.php
@@ -11,8 +11,31 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(empty($_POST["primeiroNome"]) || empty($_POST["sobreNome"]) || empty($_POST["email"]) || empty($_POST["senha"]) ){
         echo "<script>alert('Tem de preencher todos os campos necessários para continuar!'); window.history.back();</script>";
         exit;
-
     }
+
+    $primeiroNome = trim($_POST['']);
+    $sobreNome = trim($_POST['']);
+    $email = trim($_POST['']);
+    $password = ($_POST['']);
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "<script>alert('O email inserido é invalido!'); window.history.back(); </script>";
+        exit; 
+    }
+
+    $passwordHash = hash('sha256', $password);
+
+    $stmt = $conn->prepare("SELECT * FROM user WHERE Email = ?");
+    $stmt -> bind_param("s", $email);
+    $stmt -> execute();
+    $result = $stmt -> get_result();
+
+    if($result -> num_rows > 0) {
+        echo "<script>alert('Este endereço de e-mail ja esta a ser usado'); window.history.back(); </script>";
+        exit;
+    }
+
+
 }
 
 ?>
