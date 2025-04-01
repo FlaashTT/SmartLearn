fc84e6121cb47525139eb434a27aeae0e3beac58
ruben bras <ru3enbras@gmail.com>
2025-04-01 14:27:03 +0100

    logs work

--- src/logs.php
+++ src/logs.php
@@ -1,31 +1,60 @@
 <?php
 include('../database/basedados.sql');
-function criarLogs($tipoLog , $idUser){//devemos por mais campos,ter de ver depois oq vai ser necesario
+function criarLogs($tipoLog, $idUser, $saldo, $idCurso)
+{ //devemos por mais campos,ter de ver depois oq vai ser necesario
     global $conn;
 
     date_default_timezone_set("Europe/Lisbon");
     $DataAtual = date("Y-m-d H:i:s");
-   
 
-    switch ($tipoLog){
-        
-    case "Novo Registo":
-        $descricaoLog = "Foi criado um novo utilizador no sistema!"; 
-    break;
 
+    switch ($tipoLog) {
+
+        case "Novo Registo":
+            $descricaoLog = "Foi criado um novo utilizador no sistema!";
+            break;
+
+        case "Levantamento de saldo":
+            $descricaoLog = "Foi levantado saldo no valor de " . $saldo . " €(euros)";
+            break;
+
+        case "Deposito de saldo":
+            $descricaoLog = "Foi depositado na conta o valor de " . $saldo . "€euros";
+            break;
+
+
+        case "Reembolso curso":
+            $descricaoLog = "Foi solicitado reembolso do curso com id" . $idCurso;
+            break;
+
+        case "Compra curso":
+            $descricaoLog = "O utilizador comprou o curso com id" . $idCurso;
+            break;
+
+        case "Alteração de dados":
+            $descricaoLog = "Foram alterados dados de conta deste utilizador";
+            break;
+
+        case "Conta eliminada":
+            $descricaoLog = "A conta deste utilizador foi eliminada";
+            break;
+
+        case "Novo curso":
+            $descricaoLog = "Foi adicionado um novo curso";
+            break;
+
+        case "Alteração de curso":
+            $descricaoLog = "Foram alteradas informações do curso com id" . $idCurso;
+            break;
     }
+    $stmt = $conn->prepare("INSERT INTO logs_sistema (Id_user, Descricao_log, Tipo_log, Data_log) VALUES  (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $idUser, $descricaoLog, $tipoLog, $DataAtual);
 
 
-    $stmt = $conn -> prepare ("INSERT INTO logs_sistema (Id_user, Descricao_log, Tipo_log, Data_log) VALUES  (?, ?, ?, ?)");
-    $stmt -> bind_param("ssss", $idUser, $descricaoLog, $tipoLog, $DataAtual);
-
-    
-    if(!$stmt -> execute()){
-        echo"<script>alert('Erro ao criar log')</script>";
+    if (!$stmt->execute()) {
+        echo "<script>alert('Erro ao criar log')</script>";
     }
 
     $stmt->close();
     $conn->close();
-
 }
-?>
